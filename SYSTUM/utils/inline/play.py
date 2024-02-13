142f92c27069134cbd4735da2e6e410c612e612a
prashantsahlot <156554454+prashantsahlot@users.noreply.github.com>
2024-02-13 12:20:02 +0530

    play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -69,7 +69,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/prashant_sahlot",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/+919582898743",
             ),
             InlineKeyboardButton(
                 text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/Frozensupport1",
@@ -91,7 +91,7 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/prashant_sahlot",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/+919582898743",
             ),
             InlineKeyboardButton(
                 text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/Frozensupport1",
